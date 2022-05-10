21625c0681dea5c0f029e8436a0cc41979c96a42
giuseppeDiStefano001 <104845724+giuseppeDiStefano001@users.noreply.github.com>
2022-05-10 10:52:03 +0200

    Creazione metodi inizializza, creaThread, signal

--- problemaFilosofi.cpp
+++ problemaFilosofi.cpp
@@ -1,34 +1,58 @@
 #include <iostream>
 #include <pthread.h>
 #include <unistd.h>
+#include <semaphore.h>
 
 using namespace std;
 
 int const NUMERO_FILOSOFI = 4;
-pthread_t filosofi[NUMERO_FILOSOFI];
+
 int statoFilosofo[NUMERO_FILOSOFI];
+sem_t semaforo_binario s[NUMERO_FILOSOFI]; //dichiarazione semaforo binario (ogni filosofo ha un semaforo)
+pthread_t filosofi[NUMERO_FILOSOFI]; //dichiarazione array di thread di filosofi 
 const int PENSA = 0;
 const int MANGIA = 1;
 const int AFFAMATO = 2;
 
 
 void schermataIniziale();
+void inizializza();
+void creaThread(pthread_t filosofi, int &numForchette);
 int generaDurataInMs(int min, int max);
 int filosofoDestro(int i);
-inr filosofoSinistro(int i);
-void inizializza();
+int filosofoSinistro(int i);
+void prendiForchetta(int i, int *numForchette);
+
 void wait(int& s);
 
-int main()
-{
+int main(){
 	int numForchette = NUMERO_FILOSOFI;
 	inizializza();
 	schermataIniziale();
-	
-
+	while(true){
+		creaThread(NUMERO_FILOSOFI, numForchette);
+	}
+	pensa()
 	return 0;
 }
 
+
+//Metodo che: imposta stato filosofi a pensa e inizializza semafori 
+void inizializza(){
+	sem_init(semaforo_binario, 0,0); //inizializzazione dei semafori
+	for(int i = 0; i < NUMERO_FILOSOFI; i++){
+		statoFilosofo[i] = PENSA:
+	}
+}
+
+//Metodo crea thread per tutti i filosofi
+void creaThread(pthread_t filosofi[NUMERO_FILOSOFI], int &numForchette){
+	for(pos = 0; pos < NUMERO_FILOSOFI; pos++){
+		pthread_create(&filosofi[i], NULL, filosofo, (*void) i);
+	}
+}
+
+//Metodo di schermata iniziale
 void schermataIniziale(){
 	cout << "**** PROBLEMA DEI FILOSOFI A CENA ****" << endl;
 	cout << "*  Corso di Sistemi Operativi        *" << endl;
@@ -53,11 +77,7 @@ int filosofoSinistro(int i){
 	return (i + NUMERO_FILOSOFI - 1) % NUMERO_FILOSOFI;    
 }
 
-void inizializza(){
-	for(int i = 0; i < NUMERO_FILOSOFI; i++){
-		statoFilosofo[i] = PENSA:
-	}
-}
+
 
 void filosofoStaMangiando(int i, int *numForchette){
 	int destro = filosofoDestro(i);
@@ -68,8 +88,21 @@ void filosofoStaMangiando(int i, int *numForchette){
 	}
 }
 
+void prendiForchetta(int i, int *numForchette){
+	statoFilosofo[i] = AFFAMATO;
+	cout << "Il filosofo in pos: " + i + " e' affamato";
+	filosofoStaMangiando[i];
+	
+}
+
+
+
 void wait(int& s){
 	while(s < 0)
 		//pthread_exit(0);
 	s--;
+}
+
+void signal(int& s){
+	s++;
 }
