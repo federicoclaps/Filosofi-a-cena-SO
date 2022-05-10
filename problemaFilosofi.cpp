771da8a0b2b8a209017be8723a6cbd5864cf792b
giuseppeDiStefano001 <104845724+giuseppeDiStefano001@users.noreply.github.com>
2022-05-10 12:03:29 +0200

    Creazione metodo Pensa e aggiustamenti su errori di compilazione

--- problemaFilosofi.cpp
+++ problemaFilosofi.cpp
@@ -8,7 +8,7 @@ using namespace std;
 int const NUMERO_FILOSOFI = 4;
 
 int statoFilosofo[NUMERO_FILOSOFI];
-sem_t S[NUMERO_FILOSOFI]; //dichiarazione semaforo binario (ogni filosofo ha un semaforo)
+sem_t S = NUMERO_FILOSOFI; //dichiarazione semaforo binario (ogni filosofo ha un semaforo)
 pthread_t filosofi[NUMERO_FILOSOFI]; //dichiarazione array di thread di filosofi 
 const int PENSA = 0;
 const int MANGIA = 1;
@@ -17,20 +17,20 @@ const int AFFAMATO = 2;
 
 void schermataIniziale();
 void inizializza();
-void creaThread(pthread_t filosofi, int &numForchette);
+void creaThread(pthread_t filosofi);
 int generaDurataInMs(int min, int max);
 int filosofoDestro(int i);
 int filosofoSinistro(int i);
-void prendiForchette(int i, int *numForchette);
-void rilasciaForchette(int i, int *numForchette);
-void *filosofo(void *i, int *numForchette);
+void prendiForchette(int i);
+void rilasciaForchette(int i);
+void *filosofo(void *i);
 void pensa(int i);
 void mangia(int i);
 void wait(int& s);
 void signal(int& s);
 
 int main(){
-	int numForchette = NUMERO_FILOSOFI;
+	//int numForchette = NUMERO_FILOSOFI;
 	inizializza();
 	schermataIniziale();
 	while(true){
@@ -42,14 +42,14 @@ int main(){
 
 //Metodo che: imposta stato filosofi a pensa e inizializza semafori 
 void inizializza(){
-	sem_init(S, 0,0); //inizializzazione dei semafori
+	sem_init(S, 0,4); //inizializzazione dei semafori a 4
 	for(int i = 0; i < NUMERO_FILOSOFI; i++){
 		statoFilosofo[i] = PENSA;
 	}
 }
 
 //Metodo crea thread per tutti i filosofi
-void creaThread(pthread_t filosofi[NUMERO_FILOSOFI], int &numForchette){
+void creaThread(pthread_t filosofi[NUMERO_FILOSOFI]){
 	for(int i = 0; i < NUMERO_FILOSOFI; i++){
 		pthread_create(&filosofi[i], NULL, filosofo, (void *)i);
 	}
@@ -63,69 +63,76 @@ void schermataIniziale(){
 	return;
 }
 
-// metodo per la generazione automatica di intervalli di tempo (in ms)
+// Metodo per la generazione automatica di intervalli di tempo (in ms)
 int generaDurataInMs(int min, int max){
 	int durataCasuale = rand () % min + max;
     return durataCasuale;
 }
 
-// metodi che restituiscono la posizione dei filosofi a destra e a sinistra
-// siccome i filosofi si trovano in un tavolo rotondo , il filosofo in pos. 5 avrà alla sua desta il filosofo in pos. 1
+//Metodo che restituisce il filosofo a destra del filosofo in pos. i
+// ES: (il filosofo in pos. 5 avrà alla sua desta il filosofo in pos. 1)
 int filosofoDestro(int i){
 	return (i + 1) % NUMERO_FILOSOFI;
 }
 
-//il filosofo in pos. 5 avrà alla sua sinistra il filosofo in pos. 4
+//Metodo che restituisce il filosofo a sinistra del filosofo in pos. i
+//ES: (il filosofo in pos. 5 avrà alla sua sinistra il filosofo in pos. 4)
 int filosofoSinistro(int i){
 	return (i + NUMERO_FILOSOFI - 1) % NUMERO_FILOSOFI;    
 }
 
-void filosofoStaMangiando(int i, int *numForchette){
+//Metodo che imposta lo stato a Mangia e rilascia forchetta
+void filosofoStaMangiando(int i){
 	int destro = filosofoDestro(i);
 	int sinistro = filosofoSinistro(i);
 	if( (statoFilosofo[i] == AFFAMATO) && (statoFilosofo[destro] != MANGIA) && (statoFilosofo [sinistro] != MANGIA) ){
 		statoFilosofo[i] = MANGIA;
-		wait(*numForchette); 
+		sem_post(&S); 
 	}
 }
 
-void prendiForchette(int i, int *numForchette){
+//Metodo che 
+void prendiForchette(int i){
 	statoFilosofo[i] = AFFAMATO;
 	cout << "Il filosofo in pos: " << i << " e' affamato";
-	filosofoStaMangiando(i, numForchette);	
+	filosofoStaMangiando(i);	
+	sem_wait(&S);
+}
+
+void rilasciaForchette(int i){
+	statoFilosofo[i] = PENSA;
+	int sinistro = filosofoSinistro(i);
+	int destro = filosofoDestro(i);
+	filosofoStaMangiando(sinistro);
+	filosofoStaMangiando(destro);
 }
 
 void pensa(int i){
-
+	int durata  = generaDurataInMs(400,600);
+	cout << "Il filosofo in posizione: " << i << " PENSA" << "per:" << durata << "(ms)" << endl;
+	usleep(durata);
 }
 
 void mangia(int i){
 
 }
 
-void rilasciaForchette(int i, int *numForchette){
-	statoFilosofo[i] = PENSA;
-	int sinistro = filosofoSinistro(i);
-	int destro = filosofoDestro(i);
-	filosofoStaMangiando(sinistro,*numForchette);
-	filosofoStaMangiando(destro);
-}
 
-void *filosofo(void *i, int *numForchette) { 
+void *filosofo(void *i) { 
   while (true) {          
     pensa(*(int*)i);             
-    prendiForchette(*(int*)i, numForchette);        
+    prendiForchette(*(int*)i);        
     mangia(*(int*)i);               
-    rilasciaForchette(*(int*)i, numForchette);         
+    rilasciaForchette(*(int*)i);         
   }
 }
 
-void wait(int& s){
-	while(s < 0)
-		//pthread_exit(0);
-	s--;
-}
+//void wait(int& s){
+//	while(s < 0)
+//		//pthread_exit(0);
+//	s--;
+//}
 
-void signal(int& s){
-	s++;
-}
+//void signal(int& s){
+//	s++;
+//}
